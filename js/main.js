6365033c273107a2b1e4bf2cc107c35d2640548d
R1PeR <kozy@op.pl>
2017-06-03 13:02:29 +0200

    Basic portfolio page built using bootstrap

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$("a.scroll-to").on('click', function(){
+  var scrollTo = $(this).attr('data-scroll-to');
+
+  $('body, html').animate({
+
+    "scrollTop": $("#"+scrollTo).offset().top-20
+  }, 1000 );
+  return false;
+ })
